9734580f8c7a04c27b9d0f32c8eaccb03ce63e42
LunaDR <128839451+LunaDR@users.noreply.github.com>
2023-12-08 23:09:52 -0300

    Add files via upload

---
+++ tp6.js
@@ -0,0 +1,23 @@
+//LUNA DE RITO
+//COM. 5
+
+let aventura
+
+
+  function setup() {
+  createCanvas(windowWidth, windowHeight);
+  aventura = new Aventura ();
+}
+
+
+function draw() {
+  aventura.actualizar();
+}
+
+function keyPressed() {
+  aventura.juego.personaje.keyPressed();
+}
+
+function keyReleased() {
+  aventura.juego.personaje.keyReleased();
+}
